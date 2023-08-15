325be49777e68d0c015c2c3b1756518e17d2b5c6
volodie <119340399+volodie@users.noreply.github.com>
2023-08-15 12:32:34 +0300

    OtusHomeWork9

---
+++ OtusHomeWork9/ImageDownloader.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+
+namespace OtusHomeWork9
+{
+    internal class ImageDownloader
+    {
+        public delegate void ImageDownloadHandler(string message);
+        public event ImageDownloadHandler? Notify;
+        public async void Download(string urlImage)
+        {
+
+            string fileName = "bigimage.jpg";
+            var myWebClient = new WebClient();
+            //Notify?.Invoke($"Скачивание файла началось: {urlImage}");
+            //Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, urlImage);
+            var downloadTask =  myWebClient.DownloadFileTaskAsync(urlImage, fileName);
+            //Notify?.Invoke($"Скачивание файла закончилось: {urlImage}");
+            //Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, urlImage);
+            Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания ");
+            var pressedButton = Console.ReadLine();
+            if (pressedButton != "A")
+            {
+                if (downloadTask.IsCompleted) 
+                {
+                    await Console.Out.WriteLineAsync("True");
+                }
+                else
+                { await Console.Out.WriteLineAsync("False");
+                }
+            } 
+        }
+    }
+}
---
+++ OtusHomeWork9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using OtusHomeWork9;
+
+internal class Program
+{
+    private const string remotUrl = "https://img1.akspic.ru/attachments/crops/9/9/5/2/7/172599/172599-nacionalnyj_park_banf-morennoe_ozero-priroda-nacionalnyj_park-banff-7680x4320.jpg";
+    
+    private static void Main(string[] args)
+    {
+        void DisplayMessage(string message) => Console.WriteLine(message);
+        ImageDownloader imageDownloader = new ImageDownloader();
+        //imageDownloader.Notify += DisplayMessage;
+        imageDownloader.Download(remotUrl);
+        //imageDownloader.Notify -= DisplayMessage;
+    }
+}
